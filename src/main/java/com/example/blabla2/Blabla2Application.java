40e393be953f3a85d51b75c2d44e7425b78e5c6f
Jothom2912 <johannes1504@gmail.com>
2024-01-05 14:00:46 +0100

    bla

---
+++ src/main/java/com/example/blabla2/Blabla2Application.java
@@ -0,0 +1,17 @@
+package com.example.blabla2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Blabla2Application
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Blabla2Application.class, args);
+    }
+
+    //bla
+
+}
